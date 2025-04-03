f5b0634d9c3df234cc4396831ad9c9c36dbbe46f
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-04-03 15:01:34 +0200

    first commit

---
+++ src/components/PostPage.jsx
@@ -0,0 +1,12 @@
+export default function PostPage() {
+    return (
+        <>
+            <h1>Welcome to the Post Page!</h1>
+            <div className="container">
+
+            </div>
+
+        </>
+
+    )
+}
